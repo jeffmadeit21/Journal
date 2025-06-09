321a52c63468a55075f17e8d091f6e48da0dc14d
jeffmadeit21 <jeffbokor1@gmail.com>
2025-06-09 10:09:41 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,41 @@
 const CACHE_NAME = 'journal-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/icon-192.png',
-  '/icon-512.png'
+const FILES_TO_CACHE = [
+  '/Journal/',
+  '/Journal/index.html',
+  '/Journal/app.js',
+  '/Journal/manifest.json',
+  '/Journal/icon-192.png',
+  '/Journal/icon-512.png',
 ];
 
-// Install the service worker and cache files
+// Install event
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-// Serve cached content when offline
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// Update the cache if service worker is activated
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys()
-      .then(cacheNames =>
-        Promise.all(
-          cacheNames.map(cacheName => {
-            if (!cacheWhitelist.includes(cacheName)) {
-              return caches.delete(cacheName);
-            }
-          })
-        )
-      )
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/Journal/index.html'))
   );
 });
